5c220028796d44c0f6b26607ae98a0ee849828b8
John Hotovy <john.hotovy@gmail.com>
2012-06-21 17:32:51 -0600

    Add libtheora stuff to config_make_everything.sh

--- Project/jni/config_make_everything.sh
+++ Project/jni/config_make_everything.sh
@@ -6,5 +6,15 @@ function die {
 
 ./configure_x264.sh || die "X264 configure"
 ./make_x264.sh || die "X264 make"
+
+./configure_ogg.sh || die "OGG configure"
+./make_ogg.sh || die "OGG make"
+
+./configure_vorbis.sh || die "VORBIS configure"
+./make_vorbis.sh || die "VORBIS make"
+
+./configure_theora.sh || die "THEORA configure"
+./make_theora.sh || die "THEORA make"
+
 ./configure_ffmpeg.sh || die "FFMPEG configure"
 ./make_ffmpeg.sh || die "FFMPEG make"
